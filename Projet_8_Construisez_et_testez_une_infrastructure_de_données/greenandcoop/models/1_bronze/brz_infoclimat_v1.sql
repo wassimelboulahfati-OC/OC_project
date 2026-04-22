f937b23bc259caf8c9018e4277a0182db759296e
WassimElBoulahfatiWeldom <wassim.elboulahfati@weldom.fr>
2026-04-22 11:55:56 +0200

    feat: Refactor weather data SQL models to include deduplication logic and improve data selection

--- Projet_8_Construisez_et_testez_une_infrastructure_de_données/greenandcoop/models/1_bronze/brz_infoclimat_v1.sql
+++ Projet_8_Construisez_et_testez_une_infrastructure_de_données/greenandcoop/models/1_bronze/brz_infoclimat_v1.sql
@@ -2,6 +2,32 @@ with source as (
 
   select * from {{ source('raw_weather', 'infoclimat_raw') }}
 
+),
+
+deduped as (
+
+    SELECT
+        id_station,
+        dh_utc,
+        temperature,
+        pression,
+        humidite,
+        point_de_rosee,
+        visibilite,
+        vent_moyen,
+        vent_rafales,
+        vent_direction,
+        pluie_1h,
+        pluie_3h,
+        neige_au_sol,
+        nebulosite,
+        temps_omm,
+        ROW_NUMBER() OVER (
+            PARTITION BY id_station, dh_utc
+            ORDER BY _airbyte_extracted_at DESC
+        ) AS rn
+    FROM source
+
 )
 
 SELECT
@@ -20,4 +46,5 @@ SELECT
     neige_au_sol,
     nebulosite,
     temps_omm
-FROM source
+FROM deduped
+WHERE rn = 1
--- Projet_8_Construisez_et_testez_une_infrastructure_de_données/greenandcoop/models/1_bronze/brz_wu_ichtegem_v1.sql
+++ Projet_8_Construisez_et_testez_une_infrastructure_de_données/greenandcoop/models/1_bronze/brz_wu_ichtegem_v1.sql
@@ -2,22 +2,49 @@ with source as (
 
     select * from {{ source('raw_weather', 'wu_ichtegem_raw') }}
 
+),
+
+deduped as (
+
+    SELECT
+        id_station,
+        dh_utc,
+        date,
+        "Time"          AS time,
+        "Temperature"   AS temperature,
+        "Dew_Point"     AS dew_point,
+        "Humidity"      AS humidity,
+        "Wind"          AS wind,
+        "Speed"         AS speed,
+        "Gust"          AS gust,
+        "Pressure"      AS pressure,
+        "Precip__Rate_" AS precip__rate_,
+        "Precip__Accum_" AS precip__accum_,
+        "UV"            AS uv,
+        "Solar"         AS solar,
+        ROW_NUMBER() OVER (
+            PARTITION BY id_station, dh_utc
+            ORDER BY _airbyte_extracted_at DESC
+        ) AS rn
+    FROM source
+
 )
 
 SELECT
     id_station,
     dh_utc,
     date,
-    "Time"          AS time,
-    "Temperature"   AS temperature,
-    "Dew_Point"     AS dew_point,
-    "Humidity"      AS humidity,
-    "Wind"          AS wind,
-    "Speed"         AS speed,
-    "Gust"          AS gust,
-    "Pressure"      AS pressure,
-    "Precip__Rate_" AS precip__rate_,
-    "Precip__Accum_" AS precip__accum_,
-    "UV"            AS uv,
-    "Solar"         AS solar
-FROM source
+    time,
+    temperature,
+    dew_point,
+    humidity,
+    wind,
+    speed,
+    gust,
+    pressure,
+    precip__rate_,
+    precip__accum_,
+    uv,
+    solar
+FROM deduped
+WHERE rn = 1
